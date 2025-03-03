c8cad96f9cc7195297ca3cd6a11e08f211df6009
Natan Pires <natan.void@hotmail.com>
2025-03-03 17:03:39 -0300

    test: Add h2 DB

--- src/test/java/com/bookstore/jpa/BaseTestConfig.java
+++ src/test/java/com/bookstore/jpa/BaseTestConfig.java
@@ -1,6 +1,7 @@
 package com.bookstore.jpa;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,9 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseTestConfig {
+
+    @Autowired
+    protected EntityManager entityManager;
 
     @Autowired
     protected MockMvc mockMvc;
